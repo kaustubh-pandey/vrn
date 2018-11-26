664dc90472a4228b511669168cb555cf3099692b
Kaustubh Pandey <kaustubh.p16@iiits.in>
2018-11-26 19:21:50 +0530

    Delete download.sh

--- vrn/download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "This script will download the VRN network model."
-
-wget -O vrn-unguided.t7 \
-     http://cs.nott.ac.uk/~psxasj/download.php?file=vrn-unguided.t7
-wget -O face-alignment/2D-FAN-300W.t7 \
-     https://www.adrianbulat.com/downloads/FaceAlignment/2D-FAN-300W.t7
