1d4dac1e7b42bc87580efa7f2ba4d1864e0431e1
Kaustubh Pandey <kaustubh.p16@iiits.in>
2018-11-26 19:23:57 +0530

    Update vis.py

--- vrn/vis.py
+++ vrn/vis.py
@@ -20,22 +20,19 @@ im = vv.imread(args.image)
 t = vv.imshow(im)
 t.interpolate = True # interpolate pixels
 
-# volshow will use volshow3 and rendering the isosurface if OpenGL
-# version is >= 2.0. Otherwise, it will show slices with bars that you
-# can move (much less useful).
+
 volRGB = np.stack(((vol > 1) * im[:,:,0],
                    (vol > 1) * im[:,:,1],
                    (vol > 1) * im[:,:,2]), axis=3)
 
 v = vv.volshow(volRGB, renderStyle='iso')
-v.transformations[1].sz = 0.5 # Z was twice as deep during training
-
+v.transformations[1].sz = 0.5 
 l0 = vv.gca()
-l0.light0.ambient = 0.9 # 0.2 is default for light 0
-l0.light0.diffuse = 1.0 # 1.0 is default
+l0.light0.ambient = 0.9 
+l0.light0.diffuse = 1.0 
 
 a = vv.gca()
-a.camera.fov = 0 # orthographic
+a.camera.fov = 0 
 
 vv.use().Run()
 
